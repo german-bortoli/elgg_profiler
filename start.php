11b9622cc4217711437a3088a1f0232532cbccec
German <german@localhost.localdomain>
2012-05-22 00:09:06 -0300

    Elgg 1.8 integration

--- start.php
+++ start.php
@@ -1,46 +1,42 @@
 <?php
-/**
-* elgg_profiler
-*
-* @author German Bortoli & Diego Gallardo
-* @link http://community.elgg.org/pg/profile/pedroprez
-* @copyright (c) Keetup 2010
-* @link http://www.keetup.com/
-* @license GNU General Public License (GPL) version 2
-*/
 
-require_once(dirname(__FILE__) . '/vendors/plugin/plugin.php');
+/**
+ * elgg_profiler
+ *
+ * @author German Bortoli & Diego Gallardo
+ * @link http://community.elgg.org/pg/profile/pedroprez
+ * @copyright (c) Keetup 2010
+ * @link http://www.keetup.com/
+ * @license GNU General Public License (GPL) version 2
+ */
 require_once(dirname(__FILE__) . '/vendors/pqp/classes/PhpQuickProfiler.php');
-require_once(dirname(__FILE__). '/classes/ProfilerClass.php');
+require_once(dirname(__FILE__) . '/classes/ProfilerClass.php');
 
 function elgg_profiler_init() {
-	//Initializate the plugin
-	elgg_profiler_initializateplugin();
-			
-	//Page Handler
-	register_page_handler('elgg_profiler','elgg_profiler_page_handler');
-	$pqp = new PQPExample();
-$pqp->init();
+    global $PQP;
+    $PQP = new PQPExample();
+    
+    //Page Handler
+//    register_page_handler('elgg_profiler', 'elgg_profiler_page_handler');
+    elgg_extend_view('footer/analytics', 'elgg_profiler/profiler_footer');
+    elgg_register_css('profiler.css', 'mod/elgg_profiler/vendors/pqp/css/pQp.css');
 }
 
-function elgg_profiler_page_handler($page) {
-	global $CONFIG;
-	if (isset($page[0])) {
-		switch($page[0]) {
-			case "admin":
-				!@include_once(dirname(__FILE__) . "/pages/admin_p.php");
-				return false;
-          		break;
-		}
-	}
-}
+//function elgg_profiler_page_handler($page) {
+//    global $CONFIG;
+//    if (isset($page[0])) {
+//        switch ($page[0]) {
+//            case "admin":
+//                !@include_once(dirname(__FILE__) . "/pages/admin_p.php");
+//                return false;
+//                break;
+//        }
+//    }
+//}
 
 function elgg_profiler_setup() {
-	global $CONFIG;
-	if (get_context()=='admin') {
-    	add_submenu_item(elgg_echo("elgg_profiler:admin"), $CONFIG->wwwroot . "pg/elgg_profiler/admin" );
-	}
+    elgg_load_css('profiler.css');
 }
 
-register_elgg_event_handler('init','system','elgg_profiler_init');
-register_elgg_event_handler('pagesetup','system','elgg_profiler_setup');
+elgg_register_event_handler('init', 'system', 'elgg_profiler_init');
+elgg_register_event_handler('pagesetup', 'system', 'elgg_profiler_setup');
---
+++ views/default/elgg_profiler/profiler_footer.php
@@ -0,0 +1,4 @@
+<?php
+
+global $PQP;
+$PQP->init();
